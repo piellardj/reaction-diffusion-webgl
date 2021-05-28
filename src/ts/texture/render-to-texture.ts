f975e4e3ecbfc790589ce875fe85546676aed2b5
Jérémie Piellard <jeremie.piellard@gmail.com>
2021-05-28 19:53:11 +0200

    Explicit texture filter

--- src/ts/texture/render-to-texture.ts
+++ src/ts/texture/render-to-texture.ts
@@ -23,6 +23,7 @@ class RenderToTexture {
 
             const format = gl.RGBA;
             gl.texImage2D(gl.TEXTURE_2D, 0, format, wantedWidth, wantedHeight, 0, format, gl.UNSIGNED_BYTE, null);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
